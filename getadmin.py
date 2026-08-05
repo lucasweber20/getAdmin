749c3c3dd54d932a408c06ffeb62ac2e3cdd57de
lucasweber20 <lucasweber449@gmail.com>
2026-08-05 11:00:56 -0300

    Feat: add more status code

--- getadmin.py
+++ getadmin.py
@@ -30,8 +30,14 @@ def main():
         futures = [executor.submit(req.requests, url) for url in admin_urls]
         for future in concurrent.futures.as_completed(futures):
             result = future.result()
-            if result:
+            if result[1] >= 200 and result[1] < 300:
                 print(f"{result[0]} -> \033[92m{result[1]}\033[00m")
+            elif result[1] >= 300 and result[1] < 400:
+                print(f"{result[0]} -> \033[36m{result[1]}\033[00m")
+            elif result[1] >= 400 and result[1] < 500:
+                print(f"{result[0]} -> \033[33m{result[1]}\033[00m")
+            elif result[1] >= 500 and result[1] < 600:
+                print(f"{result[0]} -> \033[31m{result[1]}\033[00m")
 
 if __name__ == "__main__":
     main()
