25f9d0a5d0bc682c05c51a4d5d49d6fc3b8da720
lucasweber20 <lucasweber449@gmail.com>
2026-08-05 11:26:26 -0300

    Refactor: add si argument flag

--- getadmin.py
+++ getadmin.py
@@ -9,6 +9,7 @@ parser = argparse.ArgumentParser()
 
 args = parser.add_argument("-l", "--list", help="Specify file with urls, example: -l urls.txt", type=str)
 args = parser.add_argument("-t", "--thread", help="Specify threads number, example: -t 5", default=1, type=int)
+args = parser.add_argument("-si", help="Fuzzing directorie, example: -si", action='store_true')
 args = parser.add_argument("-o", "--output", help="Specify output file, example: -o outputs.txt", type=str)
 
 args = parser.parse_args()
@@ -17,6 +18,7 @@ def main():
     # Flags
     file = args.list
     thread = args.thread
+    fuzz = args.si
     output = args.output
 
     urls = URL(file)
