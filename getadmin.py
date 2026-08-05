56afb2bd20353d0339c9ab37ef4ed9d7fad0c3ec
lucasweber20 <lucasweber449@gmail.com>
2026-08-05 08:23:38 -0300

    Feat: add list argument flag

--- getadmin.py
+++ getadmin.py
@@ -1,7 +1,14 @@
+import argparse
 
+
+parser = argparse.ArgumentParser()
+
+args = parser.add_argument("-l", "--list", help="Specify file with urls, example: -l urls.txt", type=str)
+
+args = parser.parse_args()
 
 def main():
-    pass
+    file = args.list
 
 if __name__ == "__main__":
     main()
