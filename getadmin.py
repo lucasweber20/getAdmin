4575cdd0935fba3d58661dbba2bb6fcc5d53e874
lucasweber20 <lucasweber449@gmail.com>
2026-08-05 08:30:12 -0300

    Feat: remove duplicates

--- getadmin.py
+++ getadmin.py
@@ -1,14 +1,24 @@
 import argparse
+from scripts.URL import URL
 
 
 parser = argparse.ArgumentParser()
 
+args = parser.add_argument("-u", "--url", help='Specify url, example: -u https://example.com/?param=value', nargs="+", type=str)
 args = parser.add_argument("-l", "--list", help="Specify file with urls, example: -l urls.txt", type=str)
 
 args = parser.parse_args()
 
 def main():
+    # Flags
+    url = args.url
     file = args.list
+
+    urls = URL(url, file)
+
+    # Remove duplicates
+    if file:
+        url = urls.remove_duplicates()
 
 if __name__ == "__main__":
     main()
