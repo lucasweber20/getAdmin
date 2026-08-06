c4768ebfbc648cbe3872feaab4302a1326e72976
lucasweber20 <lucasweber449@gmail.com>
2026-08-06 13:24:17 -0300

    Refactor: add .jpeg to blacklist

--- scripts/Parser.py
+++ scripts/Parser.py
@@ -6,7 +6,7 @@ class Parser:
         self.url = url
 
     def blacklist_ext(self):
-        blacklist_ext = [".css", ".pdf", ".jpg", ".png", ".svg"]
+        blacklist_ext = [".css", ".pdf", ".jpg", ".png", ".jpeg", ".svg"]
         _, url_extension = splitext(self.url)
         if url_extension in blacklist_ext:
             return False
