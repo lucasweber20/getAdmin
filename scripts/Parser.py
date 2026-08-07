03e97303553080cee922d791fb90f2bb82eb0dd7
lucasweber20 <lucasweber449@gmail.com>
2026-08-07 07:48:58 -0300

    Refactor: add extension to blacklist extensions

--- scripts/Parser.py
+++ scripts/Parser.py
@@ -6,7 +6,7 @@ class Parser:
         self.url = url
 
     def blacklist_ext(self):
-        blacklist_ext = [".js", ".css", ".pdf", ".jpg", ".png", ".jpeg", ".svg", ".eot", ".ttf", ".woff", ".woff2"]
+        blacklist_ext = [".js", ".css", ".pdf", ".jpg", ".png", ".jpeg", ".svg", ".ico", ".eot", ".ttf", ".woff", ".woff2"]
         _, url_extension = splitext(self.url)
         if url_extension in blacklist_ext:
             return False
